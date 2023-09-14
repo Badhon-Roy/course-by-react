3c8dbd2d8cb83eec941562e7cea46338a12b2cc8
Badhon-Roy <roybadhon286@gmail.com>
2023-09-14 23:58:04 +0600

    credit hour remaining and total credit hour in carts section is update

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,44 @@ import Carts from './Component/Carts/Carts'
 import Courses from './Component/Courses/Courses'
 
 function App() {
-  const [courseTitle , SetCourseTitle] = useState([]);
+  const [courseTitle, SetCourseTitle] = useState([]);
+  const [remaining, setRemaining] = useState(20);
+  const [credit, setCredit] = useState(0)
 
-  const handleSelectBtn = title =>{
-    const newTitle = ([...courseTitle , title])
-    SetCourseTitle(newTitle)
-    
+  const handleSelectBtn = title => {
+    let cost = title.credit;
+    const newTitle = ([...courseTitle, title])
+    const isSelect = courseTitle.find(item => item.id === title.id)
+    if (isSelect) {
+      return alert('this is clicked')
+    }
+    else {
+      courseTitle.forEach(cart => {
+        cost += cart.credit;
+      })
+      const total = 20 - cost;
+      if(total<0){
+        return alert('beshi')
+      }
+      else{
+        setRemaining(total)
+        SetCourseTitle(newTitle)
+      }
+    }
+    setCredit(cost)
+
   }
-  
+
 
   return (
     <div className='max-w-[1700px] mx-auto lg:px-16 md:px-8 px-4 my-8'>
       <h1 className='text-center md:text-4xl text-2xl font-bold '>Course Registration</h1>
       <div className='md:flex gap-3'>
         <Courses handleSelectBtn={handleSelectBtn}></Courses>
-        <Carts courseTitle={courseTitle}></Carts>
+        <Carts courseTitle={courseTitle}
+          remaining={remaining}
+          credit={credit}
+        ></Carts>
       </div>
     </div>
   )
--- src/Component/Carts/Carts.jsx
+++ src/Component/Carts/Carts.jsx
@@ -2,22 +2,25 @@ import Cart from "../Cart/Cart";
 import PropTypes from 'prop-types'
 
 
-const Carts = ({courseTitle}) => {
+const Carts = ({courseTitle ,remaining ,credit}) => {
     return (
         <div className="md:w-1/4 my-8">
             <div className="rounded-xl bg-white p-4 shadow-lg">
-                <h3 className="text-[18px] font-bold text-[#2F80ED] border-b-2 pb-4">Credit Hour Remaining 7 hr</h3>
+                <h3 className="text-[18px] font-bold text-[#2F80ED] border-b-2 pb-4">Credit Hour Remaining {remaining} hr</h3>
                 <h2 className="text-xl font-bold my-4">Course Name</h2>
-                <h3 className="font-medium border-b-2 border-t-2 py-4">Total Credit Hour : 13</h3>
                 {
                     courseTitle.map(title=> <Cart key={title.id} title={title}></Cart>)
                 }
+                <h3 className="font-medium border-b-2 border-t-2 py-4">Total Credit Hour : {credit}</h3>
+                
             </div>
         </div>
     );
 };
 Carts.propTypes = {
-    courseTitle: PropTypes.array
+    courseTitle: PropTypes.array,
+    remaining: PropTypes.number,
+    credit: PropTypes.number
 }
 
 export default Carts;
