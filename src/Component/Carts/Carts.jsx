9b8627f1fa48b93b4015b7004b297fe766591e9b
Badhon-Roy <roybadhon286@gmail.com>
2023-09-14 23:10:56 +0600

    carts section design update and full page is responsive

--- src/Component/Carts/Carts.jsx
+++ src/Component/Carts/Carts.jsx
@@ -1,11 +1,19 @@
+import PropTypes from 'prop-types'
 
 
 const Carts = () => {
     return (
-        <div>
-            <h2>This is cart section</h2>
+        <div className="md:w-1/4 my-8">
+            <div className="rounded-xl bg-white p-4 shadow-lg">
+                <h3 className="text-[18px] font-bold text-[#2F80ED] border-b-2 pb-4">Credit Hour Remaining 7 hr</h3>
+                <h2 className="text-xl font-bold my-4">Course Name</h2>
+                <h3 className="font-medium border-b-2 border-t-2 py-4">Total Credit Hour : 13</h3>
+            </div>
         </div>
     );
 };
+Carts.propTypes = {
+    courseTitle: PropTypes.array
+}
 
 export default Carts;
