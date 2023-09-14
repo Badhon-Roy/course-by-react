c622004f0001215b2e44d665734dd4b55ca66219
Badhon-Roy <roybadhon286@gmail.com>
2023-09-14 23:23:19 +0600

    when click the select button pass the coures name in the Carts section

--- src/Component/Courses/Courses.jsx
+++ src/Component/Courses/Courses.jsx
@@ -3,7 +3,7 @@ import Course from "../Course/Course";
 import PropTypes from 'prop-types'
 
 
-const Courses = () => {
+const Courses = ({ handleSelectBtn }) => {
     const [course, setCourse] = useState([]);
     useEffect(() => {
         fetch('Data.json')
@@ -14,12 +14,14 @@ const Courses = () => {
         <div className="md:w-3/4 grid md:grid-cols-3 gap-4 my-8">
             {
                 course.map(course => <Course key={course.id}
+                handleSelectBtn={handleSelectBtn}
                 course={course}></Course>)
             }
         </div>
     );
 };
 Courses.propTypes = {
-    course: PropTypes.array
+    course: PropTypes.array,
+    handleSelectBtn: PropTypes.func
 }
 export default Courses;
